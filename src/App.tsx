c489844413758328f5834b98c12aa17cd752186b
kyzinatra <main@kyzinatra.ru>
2023-03-13 13:40:16 +0300

    add css module ts plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
+import style from "./App.module.css";
 
 function App() {
   const [count, setCount] = useState(0);
