045a6ef00c271dafa225406a43cbd8b3e3f58c19
Михаил Жегалин <mixal.zhegalin@mail.ru>
2023-05-17 20:35:05 +0300

    some fix

--- packages/frontend/src/components/App/App.component.tsx
+++ packages/frontend/src/components/App/App.component.tsx
@@ -4,11 +4,5 @@ import axios from 'axios';
 export const App: React.FC = () => {
   const [cats, setCats] = useState(0);
 
-  useEffect(() => {
-    axios.get('http://localhost:3001/get-cats-amount').then(({ data }) => {
-      setCats(Number(data));
-    });
-  }, []);
-
   return <p>cats amount: {cats}</p>;
 };
