4b5ead33d3c09dd5f97f61df9183316929a0c495
yandrinsky <57066460+yandrinsky@users.noreply.github.com>
2023-07-05 18:53:44 +0300

    fix sign-up

--- packages/backend/api/controllers/sign-up/sign-up.validator.ts
+++ packages/backend/api/controllers/sign-up/sign-up.validator.ts
@@ -1,11 +1,11 @@
 import { ISingUpDto } from './sign-up.types';
 import { isOnlyRussian } from '../../../domain/testers';
 import { CLIENT_ERRORS } from '../../../domain/errors/client-errors';
-import { User } from '../../../bd';
+import { ERoles, User } from '../../../bd';
 import { TValidator } from '../../../domain/types/validator.type';
 
 export const signUpValidator: TValidator<ISingUpDto> = async req => {
-    const { email, name, surname, patronymic } = req.body;
+    const { email, name, surname, patronymic, role } = req.body;
 
     if (!isOnlyRussian(name) || !isOnlyRussian(surname)) {
         return CLIENT_ERRORS.BAD_NAME;
@@ -19,5 +19,9 @@ export const signUpValidator: TValidator<ISingUpDto> = async req => {
 
     if (user) {
         return CLIENT_ERRORS.EMAIL_IS_ALREADY_USED;
+    }
+
+    if (role !== ERoles.User && role !== ERoles.Creator) {
+        return CLIENT_ERRORS.BAD_DTO;
     }
 };
