7ab4f442d3b978790728f6c1498c501b28c2b28c
Danil <danilsolom@mail.ru>
2023-07-11 14:43:01 +1000

    фиксим задвоение папочек

--- packages/frontend/src/router/routes.tsx
+++ packages/frontend/src/router/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { ROUTES } from '../constants/routes';
-import { Page404 } from '../pages/404/404.page';
+
+import { Error } from '@pages/error/error.page';
 
 export const REACT_ROUTER = createBrowserRouter([
   {
@@ -8,27 +9,27 @@ export const REACT_ROUTER = createBrowserRouter([
     async lazy() {
       return { Component: (await import('../pages/home/home.page')).Home };
     },
-    errorElement: <Page404 />
+    errorElement: <Error />
   },
   {
     path: ROUTES.TASKS_ROUTE,
     async lazy() {
       return { Component: (await import('../pages/home/home.page')).Home };
     },
-    errorElement: <Page404 />
+    errorElement: <Error />
   },
   {
     path: ROUTES.SIGN_IN_ROUTE,
     async lazy() {
       return { Component: (await import('../pages/sign-in/sign-in.page')).SignIn };
     },
-    errorElement: <Page404 />
+    errorElement: <Error />
   },
   {
     path: ROUTES.SIGN_UP_ROUTE,
     async lazy() {
       return { Component: (await import('../pages/sign-up/sign-up.page')).SignUp };
     },
-    errorElement: <Page404 />
+    errorElement: <Error />
   }
 ]);
